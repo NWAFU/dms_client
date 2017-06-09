c7f78d759e630dfd2f769167e9afdaa618b21718
Wu Heng <630613064@qq.com>
2017-06-09 15:38:48 +0800

    client异常类

--- client/header/client_exception.h
+++ client/header/client_exception.h
@@ -1,29 +1,75 @@
-#ifndef CLIENT_EXCEPTION_H
-#define CLIENT_EXCEPTION_H
+#ifndef CLIENTEXCEPTION_H
+#define CLIENTEXCEPTION_H
 
 #include <exception>
+#include <string>
 
+using namespace std;
+
+//0.客户端异常
 class ClientException:public exception
 {
 private:
-    string msg;
+	string msg;
 public:
-    ClientException():msg("Client Exception!")
-    {
-        Exception();
-    }
-    ClientException(string const & msg):msg("Client Exception:")
-    {
-        Exception();
-        this->msg+=msg;
-        this->msg+="!";
-    }
+	ClientException()
+	{
+		this -> msg = "客户机异常!"
+	}
+	ClientException(const string& msg)
+	{
+		this -> msg = "客户端异常————";
+		this -> msg += msg;
+		this -> msg += "!";
+	}
+	~ClientException() throw(){}
+	const char* whatMsg(){
+		return this -> msg.c_str();
+	}
 };
 
+//1.备份异常
 class BackupException:public ClientException
 {
-private:
-
+public:
+        BackupException():ClientException("备份时异常！"){}
+	BackupException(const string& msg):ClientException(msg){}
 };
 
-#endif // CLIENT_EXCEPTION_H
+//2.读取异常
+class ReadException:
+	public ClientException
+{
+public:
+        ReadException():ClientException("读取时异常！"){}
+	ReadException(const string& msg):ClientException(msg){}
+};
+
+//3.套接字异常
+class SocketException:
+	public ClientException
+{
+public:
+        SocketException():ClientException("套接字异常！"){}
+	SocketException(const string& msg):ClientException(msg){}
+};
+
+//4.发送异常
+class SendException:
+	public ClientException
+{
+public:
+        SendException():ClientException("发送时异常！"){}
+	SendException(const string& msg):ClientException(msg){}
+};
+
+//5.保存异常
+class SaveException:
+	public ClientException
+{
+public:
+        SaveException():ClientException("保存时异常！"){}
+	SaveException(const string& msg):ClientException(msg){}
+};
+
+#endif      //CLIENTEXCEPTION_H
