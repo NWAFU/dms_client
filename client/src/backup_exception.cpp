a5e46e9ad71a9bcb65117b569a90ab76734aee00
Wu Heng <630613064@qq.com>
2017-06-20 08:36:04 +0800

    6月20日，规范注释

--- client/src/backup_exception.cpp
+++ client/src/backup_exception.cpp
@@ -1,7 +1,7 @@
 #include "header/backup_exception.h"
 
 /**************************************************
-*作者：吴恒
+*作者：wu heng
 *函数名：BackupException()
 *功能：备份异常类默认构造函数
 *输入参数：none
@@ -13,10 +13,10 @@ BackupException::BackupException():ClientException("备份时异常")
 }
 
 /**************************************************
-*作者：吴恒
+*作者：wu heng
 *函数名：BackupException(const string& msg)
 *功能：备份异常类带参数构造函数
-*输入参数：const string& msg
+*输入参数：msg
 *输出参数：none
 *返回值：none
 **************************************************/
--- client/src/socket_exception.cpp
+++ client/src/socket_exception.cpp
@@ -1,7 +1,7 @@
 #include "header/socket_exception.h"
 
 /**************************************************
-*作者：吴恒
+*作者：wu heng
 *函数名：SocketException()
 *功能：套接字异常类默认构造函数
 *输入参数：none
@@ -13,10 +13,10 @@ SocketException::SocketException():ClientException("套接字异常")
 }
 
 /**************************************************
-*作者：吴恒
+*作者：wu heng
 *函数名：SocketException(const string& msg)
 *功能：套接字异常类带参数构造函数
-*输入参数：const string& msg
+*输入参数：msg
 *输出参数：none
 *返回值：none
 **************************************************/
